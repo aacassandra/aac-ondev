ca545bf0c222b06a62b7b7c295154aeeda2eb217
aacassandra <alauddinafifcassandra@programmer.net>
2020-10-21 13:09:39 +0700

    react-create-app on Heroku

---
+++ build/precache-manifest.47a6278b97a38baaa6202d7ffdc32de0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e290b55c4fb01f87628a5cbafc206564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69a28dc523e70d8b3084",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ba56d70a09f24d6db4e",
+    "url": "/static/js/2.0d75798e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0d75798e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a28dc523e70d8b3084",
+    "url": "/static/js/main.11df1324.chunk.js"
+  },
+  {
+    "revision": "9ee31af3871e9e568c3e",
+    "url": "/static/js/runtime-main.43a7c04d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
